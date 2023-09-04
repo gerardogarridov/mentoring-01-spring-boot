27db85b679ed9766b576f146ca45b5c85d098f4f
Jose Miguel Molina <jose.molinar@sonda.com>
2023-09-04 16:28:11 -0300

    Se agregan servicios para eliminar y actualizar elementos de la BD.

--- src/main/java/cl/sonda/mentoring/persona/PersonaController.java
+++ src/main/java/cl/sonda/mentoring/persona/PersonaController.java
@@ -1,35 +1,71 @@
 package cl.sonda.mentoring.persona;
+
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 @RequestMapping("/persona")
 public class PersonaController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(PersonaController.class);
+    @Value("${app.holamundo.toDelete}")
+    private String eliminar;
+    @Value("${app.holamundo.toUpdate}")
+    private String actualizar;
+
     @Autowired
     private PersonaService personaService;
-    
-    @PostMapping(produces =  MediaType.APPLICATION_JSON_VALUE, consumes  = MediaType.APPLICATION_JSON_VALUE)
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<EstadoDTO> postPersona(@RequestBody PersonaDTO personaDTO) {
-        return  new ResponseEntity<>(personaService.crearPersona(personaDTO),
+        return new ResponseEntity<>(personaService.crearPersona(personaDTO),
                 HttpStatus.OK);
     }
 
-    @GetMapping(produces =  MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<PersonaDTO>> getPersonas() {
-        return  new ResponseEntity<>(personaService.findAll(), HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<?> putPersona(@PathVariable String id, @RequestBody PersonaDTO personaDTO){
+        try{
+            LOGGER.info(String.format(actualizar, id));
+            return new ResponseEntity<>(personaService.actualizarPersona(id, personaDTO), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>("{\"error\":\"Error. Por favor intente mas tarde.\"}", HttpStatus.BAD_REQUEST);
+        }
     }
 
-    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePersona(@PathVariable String id) {
+        try {
+            LOGGER.info(String.format(eliminar, id));
+            // personaService.eliminarPersona(id);
+            // return ResponseEntity.status(HttpStatus.OK).body((String.format(eliminado, id)));
+
+            return new ResponseEntity<>(personaService.eliminarPersona(id), HttpStatus.OK);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("{\"error\":\"Error. Verifique los datos de actualizacion.\"}", HttpStatus.BAD_REQUEST);
+            //return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\":\"Error. Por favor intente mas tarde.\"}");
+        }
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<PersonaDTO>> getPersonas() {
+        return new ResponseEntity<>(personaService.findAll(), HttpStatus.OK);
+    }
+
 }
--- src/main/java/cl/sonda/mentoring/persona/PersonaService.java
+++ src/main/java/cl/sonda/mentoring/persona/PersonaService.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class PersonaService {
 
@@ -18,9 +19,54 @@ public class PersonaService {
         return new EstadoDTO("OK");
     }
 
+    public EstadoDTO actualizarPersona(String id, PersonaDTO personaDTO) throws Exception {
+        try {
+            if (personaDao.existsById(id)) {
+                PersonaEntity personaEntityAux = personaDao.findById(id).get();
+                if ((personaDTO.getRut() == null || personaDTO.getRut().isEmpty())
+                        && (personaDTO.getNombre() == null || personaDTO.getNombre().isEmpty())
+                        && (personaDTO.getApellido() == null || personaDTO.getApellido().isEmpty())) {
+                    return new EstadoDTO("NO SE ASIGNARON DATOS PARA ACTUALIZAR");
+                } else if (personaDTO.getRut() != null && !personaDTO.getRut().isEmpty()) {
+                    personaEntityAux.setRut(personaDTO.getRut());
+                }
+                personaEntityAux.setNombre(personaDTO.getNombre());
+                personaEntityAux.setApellido(personaDTO.getApellido());
+                personaDao.save(personaEntityAux);
+                return new EstadoDTO("ACTUALIZADO");
+            } else {
+                return new EstadoDTO("NO EXISTE PARA SER ACTUALIZADO");
+                // throw new Exception();
+            }
+        }catch (Exception e){
+            throw new Exception(e.getMessage());
+        }
+    }
+
     public List<PersonaDTO> findAll() {
         return personaDao.findAll().stream().map(personaEntity -> new PersonaDTO(personaEntity)).collect(Collectors.toList());
     }
 
+    /*public boolean eliminarPersona(String id){
+        PersonaEntity personaEntity = new PersonaEntity();
+        personaEntity.setRut(id);
+        personaDao.deleteById(personaEntity.getRut());
+        //personaDao.deleteById(id);
+        return true;
+    }*/
+
+    public EstadoDTO eliminarPersona(String id) throws Exception {
+        try {
+            if (personaDao.existsById(id)) {
+                personaDao.deleteById(id);
+                return new EstadoDTO("ELIMINADO");
+            } else {
+                return new EstadoDTO("NO EXISTE PARA SER ELIMINADO");
+                //throw new Exception();
+            }
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
+    }
 
 }
